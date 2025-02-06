8820f1ac7771162ddf9be5c6a8e485c1939e75d7
Jiří Strnadel <Juzba88@gmail.com>
2025-02-06 18:42:35 +0100

    funkční

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,44 +1,47 @@
-import './Slider.css';
-import data from '../data';
-import { useEffect, useState } from 'react';
-import { FaAnglesLeft } from 'react-icons/fa6';
-import { FaAnglesRight } from 'react-icons/fa6';
+import "./Slider.css";
+import data from "../data";
+import { useEffect, useState } from "react";
+import { FaAnglesLeft } from "react-icons/fa6";
+import { FaAnglesRight } from "react-icons/fa6";
+
+let buttonRight;
 
 const Slider = () => {
-    const [index, setIndex] = useState(0);
-    // const [count, setCount] = useState(0);
-    const { image, title, tags, description } = data[index];
+	const [index, setIndex] = useState(0);
+	const { image, title, tags, description } = data[index];
+	// const { image1, title1, tags1, description1 } = data[() => index + 1 > data.length - 1 ? 0 : index + 1];
 
-    console.log(index);
+	buttonRight = () => setIndex(() => (index + 1 > data.length - 1 ? 0 : index + 1));
 
-    const buttonRight = () => {
+	const buttonLeft = () => setIndex(() => (index - 1 < 0 ? data.length - 1 : index - 1));
 
-    };
-    
-    const buttonLeft = () => {};
+	useEffect(() => {
+		const t1 = setTimeout(() => buttonRight(), 3000);
+		return () => clearTimeout(t1);
+	}, [index]);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setIndex(() => {
-                const newIndex = index + 1;
-                if (newIndex > data.length - 1) return 0;
-                return newIndex;
-            });
-        }, 2000);
-    }, [index]);
-
-    return (
-        <div className='slider'>
-            <img src={image} alt=''></img>
-            <h2>{title}</h2>
-            <p>{description}</p>
-            <p>{tags}</p>
-            <div className='slider-controls'>
-                <button onClick={buttonRight}>{FaAnglesLeft()}</button>
-                <button onClick={buttonLeft}>{FaAnglesRight()}</button>
-            </div>
-        </div>
-    );
+	return (
+		<div className='slider'>
+			<div className='box'>
+				<div>
+					<img src={image} alt=''></img>
+					<h2>{title}</h2>
+					<p>{description}</p>
+					<p>{tags}</p>
+				</div>
+				<div >
+					{/* <img src={image1} alt=''></img>
+					<h2>{title1}</h2>
+					<p>{description1}</p>
+					<p>{tags1}</p> */}
+				</div>
+			</div>
+			<div className='slider-controls'>
+				<button onClick={buttonRight}>{FaAnglesLeft()}</button>
+				<button onClick={buttonLeft}>{FaAnglesRight()}</button>
+			</div>
+		</div>
+	);
 };
 
 export default Slider;
