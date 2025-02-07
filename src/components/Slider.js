429774e025daf0f9a7af27fd70110e91ef9b07d0
Jiří Strnadel <Juzba88@gmail.com>
2025-02-07 03:35:21 +0100

    done

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,47 +1,48 @@
-import "./Slider.css";
-import data from "../data";
-import { useEffect, useState } from "react";
-import { FaAnglesLeft } from "react-icons/fa6";
-import { FaAnglesRight } from "react-icons/fa6";
+import './Slider.css';
+import data from '../data';
+import { useEffect, useState } from 'react';
+import { FaAnglesLeft } from 'react-icons/fa6';
+import { FaAnglesRight } from 'react-icons/fa6';
 
-let buttonRight;
+function Slider() {
+    const [index, setIndex] = useState(0);
 
-const Slider = () => {
-	const [index, setIndex] = useState(0);
-	const { image, title, tags, description } = data[index];
-	// const { image1, title1, tags1, description1 } = data[() => index + 1 > data.length - 1 ? 0 : index + 1];
+    const slideLeft = () => setIndex(() => (index - 1 < 0 ? data.length - 1 : index - 1));
 
-	buttonRight = () => setIndex(() => (index + 1 > data.length - 1 ? 0 : index + 1));
+    const slideRight = () => setIndex(() => (index + 1 < data.length ? index + 1 : 0));
 
-	const buttonLeft = () => setIndex(() => (index - 1 < 0 ? data.length - 1 : index - 1));
+    useEffect(() => {
+       const t1 = setTimeout(() => slideRight(),5000);
+       return ()=> clearTimeout(t1)
+    });
 
-	useEffect(() => {
-		const t1 = setTimeout(() => buttonRight(), 3000);
-		return () => clearTimeout(t1);
-	}, [index]);
+    return (
+        <section className='slider'>
+            {data.map((oneMovie, indexMovie) => {
+                const { id, image, title, age, description, tags } = oneMovie;
 
-	return (
-		<div className='slider'>
-			<div className='box'>
-				<div>
-					<img src={image} alt=''></img>
-					<h2>{title}</h2>
-					<p>{description}</p>
-					<p>{tags}</p>
-				</div>
-				<div >
-					{/* <img src={image1} alt=''></img>
-					<h2>{title1}</h2>
-					<p>{description1}</p>
-					<p>{tags1}</p> */}
-				</div>
-			</div>
-			<div className='slider-controls'>
-				<button onClick={buttonRight}>{FaAnglesLeft()}</button>
-				<button onClick={buttonLeft}>{FaAnglesRight()}</button>
-			</div>
-		</div>
-	);
-};
+                let position = 'nextPosition';
+
+                if (indexMovie === index) position = 'position';
+
+                if (indexMovie === index - 1 || (index === 0 && indexMovie === data.length - 1)) position = 'lastPosition';
+
+                return (
+                    <div className='card' id={position} key={id}>
+                        <img src={image} alt=''></img>
+                        <h2>{title}</h2>
+                        <p>{description}</p>
+                        <p>{tags}</p>
+                        <p>{age}</p>
+                    </div>
+                );
+            })}
+            <div className='buttons'>
+                <button onClick={() => slideLeft()}>{FaAnglesLeft()}</button>
+                <button onClick={() => slideRight()}>{FaAnglesRight()}</button>
+            </div>
+        </section>
+    );
+}
 
 export default Slider;
